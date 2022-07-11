f2c31d49231837c491cef137cc94e8ccd60b82d9
NadyaCodes <nadyacorscadden@gmail.com>
2022-07-11 18:11:46 -0400

    add function to progress bar

--- src/progress.js
+++ src/progress.js
@@ -1,30 +1,37 @@
 import { useState } from 'react'
 import Progressbar from 'react-js-progressbar'
+import confetti from 'canvas-confetti'
 
 
 
 export default function Progress() {
-  const [percentage, setPercentage] = useState(50)
+  const [percentage, setPercentage] = useState({ Knitting: 10, Dance: 20, Coding: 50})
 
+  const activitesArray = Object.keys(percentage)
 
-  const activities = { Knitting: 10, Dance: 20, Coding: 50}
-
-  const activitesArray = Object.keys(activities)
-
-  const printObject = (object) => {
-    console.log("yay")
-
+  const changePercent = (activity) => {
+    let newNum = percentage[activity] += 10;
+    if (newNum === 100) {
+      confetti();
+    }
+    if (newNum > 100) {
+      newNum = 10;
+    }
+    const newPercentage = {...percentage}
+    newPercentage[activity] = newNum
+    setPercentage(newPercentage);
   }
 
 
+
   return (
-    <section class="allActivities">
-      {activitesArray.map(activity => 
-        <div class="activityContainer">
+    <section className="allActivities">
+      {activitesArray.map(activity =>
+        <div className="activityContainer">
           <h2>{activity}</h2>
-        <div class="progressbarContainer">
+        <div className="progressbarContainer">
           <Progressbar
-            input={activities[activity]}
+            input={percentage[activity]}
             pathWidth={10}
             pathColor={['#56ab2f', '#a8e063']}
             trailWidth={15}
@@ -32,6 +39,7 @@ export default function Progress() {
             textStyle={{ fill: 'blue' }}
           >
           </Progressbar>
+          <button onClick={() => changePercent(activity)}>Up</button>
         </div>
       </div>
       )}
